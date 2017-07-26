8a939fe5043b9892985ba5f5b9d87b158e8086c3
Ohan <ofillbach@athenahealth.com>
2017-07-26 13:59:53 -0400

    guess who's back? mr modal.

---
+++ modal.js
@@ -0,0 +1,16 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function closeModal(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
